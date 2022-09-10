7cd49940e61becd46bc3bb48a147a90a9d560b92
kien6034 <trinhtrungkientb99@gmail.com>
2022-09-10 22:28:34 +0700

    analyzer: get the author and the content

--- bot/bot.py
+++ bot/bot.py
@@ -29,3 +29,42 @@ txtPass.send_keys(password)
 txtPass.send_keys(Keys.ENTER)
 
 sleep(3)
+# Go to pages 
+browser.get("https://www.facebook.com/groups/GhienPhuQuoc")
+elm = browser.find_element(by=By.TAG_NAME, value="html")
+
+# Scroll down 
+elm.send_keys(Keys.END)
+sleep(1)
+elm.send_keys(Keys.END)
+sleep(3)
+
+
+posts = elm.find_elements(by=By.CSS_SELECTOR, value="div.g4tp4svg.mfclru0v.om3e55n1.p8bdhjjv")
+print(f"{len(posts)} posts found \n")
+
+for post in posts:
+    
+    header = post.find_element(by=By.CLASS_NAME, value="cgu29s5g")
+    sections = post.find_elements(by= By.CSS_SELECTOR, value=".jroqu855.nthtkgg5")
+    name = sections[0]
+    date = sections[1]
+
+    content = post.find_elements(by= By.CSS_SELECTOR, value=".m8h3af8h.l7ghb35v.kjdc1dyq.kmwttqpk.gh25dzvf")
+    if len(content) > 1: 
+        print(content[1].text)
+    else:
+        print(content[0].text)
+  
+
+    # time_panels =  header.find_elements(by= By.CLASS_NAME, value="f7rl1if4")
+    # for time in time_panels:
+    #     print("..")
+    #     print(time.text)
+
+    # print(name.text)
+   
+    # content = post.find_element(by=By.TAG_NAME, value="span")
+    # print(content.text)
+    #m8h3af8h l7ghb35v kjdc1dyq kmwttqpk gh25dzvf
+    #print(header.text)
